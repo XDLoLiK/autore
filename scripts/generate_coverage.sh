a6fff0e91cfaeb27130e1233de6d479928fd8666
Stanislav Goriainov <stangoryainov@gmail.com>
2023-10-02 23:10:48 +0300

    autore: Move coverage script to seperate folder
    Signed-off-by: Stanislav Goriainov <stangoryainov@gmail.com>

---
+++ scripts/generate_coverage.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+rustup component add llvm-tools-preview
+export RUSTFLAGS="-Cinstrument-coverage"
+export LLVM_PROFILE_FILE="autore-%p-%m.profraw"
+cargo build
+cargo test
+grcov . -s . --binary-path ./target/debug/ -t html --branch --ignore-not-existing -o ./target/debug/coverage/
