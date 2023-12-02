ac2f0f6baab1766aae54deaa1200336f412c7762
Stanislav Goryainov <stangoryainov@gmail.com>
2023-12-02 19:03:24 +0300

    scripts: Improve coverage generation
    Signed-off-by: Stanislav Goryainov <stangoryainov@gmail.com>

--- scripts/coverage.sh
+++ scripts/coverage.sh
@@ -1,9 +1,20 @@
 #!/bin/sh
 
+# Install the llvm-tools-preview component
 rustup component add llvm-tools-preview
+
+# Ensure that the coverage is enabled
 export RUSTFLAGS="-Cinstrument-coverage"
+
+# Build the project
+cargo build --all-features
+
+# Ensure each test runs gets its own profile information 
 export LLVM_PROFILE_FILE="autore-%p-%m.profraw"
-cargo build
+
+# Run tests
 cargo test
+
+# Generate a coverage report from coverage artifacts
 grcov . -s . --binary-path ./target/debug/ -t html --branch --ignore-not-existing -o ./target/debug/coverage/
 
